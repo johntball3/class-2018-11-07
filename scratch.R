40c017f0834880a6ddadc6adde00db9acf7847a8
John Ball <johnball@Johns-MacBook-Pro-10.local>
2018-11-07 14:10:54 -0500

    first

--- scratch.R
+++ scratch.R
@@ -10,9 +10,16 @@ library(fs)
 
 # 1. Read data/ex_926_I.csv into a tibble and provide a summary.
 
+x_926 <- read_csv("data/ex_926_I.csv")
+summary(x_926)
+
 # 2. Create a vector with all the file names in data/.
 
+file_names <- dir_ls("data/")
+
 # 3. Create a vector with just the file names that have an "A" in them.
+
+file_names <- str_subset(file_names, pattern = "A")
 
 # 4. Read in all the files into one big tibble. Check out ?map_dfr . . .
 # Background reading here:
@@ -23,8 +30,16 @@ library(fs)
 
 # 6. Find the 4 files with the largest number of observations.
 
+
 # 7. Write a function which takes a character string like "A" and then reads in
 # all the files which have "A" in the name.
+
+fun <- function(pattern) {
+  input <- 
+  file_names <- str_subset(file_names, pattern = "C")
+  x <- map_dfr(file_names, read_csv, .id = "source")
+  
+}
 
 # 8. Create a Shiny App which displays the histogram of b, allowing the user to
 # subset the display for specific values of c.
